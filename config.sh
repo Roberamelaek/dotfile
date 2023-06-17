71cc67a889306fb7c7eeff2d924e106bcaa1a853
Roberamelaek <Robaman2231999@gmail.com>
2023-06-17 15:59:31 -0400

    Fixes #3:added .alias and the installation on config file

--- config.sh
+++ config.sh
@@ -7,3 +7,15 @@ else
 	ln -s "$vimrc_path" "$home_path"
 	echo "The symboilc linking do sucssusfully"
 fi
+
+if grep -Fxq "source ~/.dotfiles/.alias" ~/.bashrc; then
+
+	echo "Checking if the connection of the file..."
+	echo "The file is already linked!."
+
+else
+	echo "Linking .alias to .bashrc"
+	echo "source ~/.dotfiles/.alias">> ~/.bashrc
+	echo "Linked sucssusfully"
+
+fi
