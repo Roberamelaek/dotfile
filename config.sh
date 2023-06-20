b265bd874fdea913543bacc05eca1d4e19ae1e6a
Roberamelaek <69109461+Roberamelaek@users.noreply.github.com>
2023-06-20 16:24:58 -0400

    Dev (#9)
    * creating a dev branch with a readme file
    
    * Update README.md
    
    make the readme helpful and better for other users
    
    * Update README.md
    
    * update dotfiles to  .dotfiles
    
    * Fixes #2
    
    * the readme file
    
    * Fixes #3:added .alias and the installation on config file
    
    * Fix (#5)(#6)

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@ vimrc_path="$HOME/.dotfiles/vimrc"
 home_path="$HOME/.vimrc"
 
 if [ -f "$home_path" ]; then
-	echo "Error:.vimrc file already exsists aborting"
+	echo ".vimrc file already exsists aborting"
 else
 	ln -s "$vimrc_path" "$home_path"
 	echo "The symboilc linking do sucssusfully"
@@ -19,3 +19,18 @@ else
 	echo "Linked sucssusfully"
 
 fi
+
+if grep -Fxq "source ~/.dotfiles/.prompt" ~/.bashrc; then
+
+	echo "checking if the connection of the file..."
+	echo "The file is already linked!."
+
+else 
+  echo "Linking .prompt to .bashrc..."
+  echo "source ~/.dotfiles/.prompt">> ~/.bashrc
+	echo "Linked sucssusfully"
+
+fi
+
+
+
