3e2a942ae04d2ab399b5ed8b16c170fa5cef7727
Roberamelaek <Robaman2231999@gmail.com>
2023-07-07 19:09:07 -0400

    #fix 11 #fix11

--- config.sh
+++ config.sh
@@ -1,12 +1,22 @@
 vimrc_path="$HOME/.dotfiles/vimrc"
 home_path="$HOME/.vimrc"
 
+gitrc_path="$HOME/.dotfiles/gitconfig"
+git_path="$HOME/.gitconfig"
+
 tmuxrc_path="$HOME/.dotfiles/tmuxrc"
 tmux_path="$HOME/.tmux.conf"
 
-[ -f "$home_path" ] && echo ".vimrc file already exists. Aborting." || ln -s "$vimrc_path" "$home_path" && echo "Symbolic linking successful."
+nbrc_path="$HOME/.dotfiles/nbrc"
+nb_path="$HOME/.nbrc"
 
-[ -f "$tmux_path" ] && echo ".tmux.conf file already exists. Aborting." || ln -s "$tmuxrc_path" "$tmux_path" && echo "symbolic linking successful."
+[ -f "$home_path" ] && echo ".vimrc file already exists. Aborting." || ln -s "$vimrc_path" $home_path && echo "Symbolic linking successful."
+
+[ -f "$nb_path" ] && echo ".nbrc file is already present." || ln -s "$nbrc_path" $nb_path && echo "nb is successfully customized."
+
+[ -f "$tmux_path" ] && echo ".tmux.conf file already exists. Aborting." || ln -s "$tmuxrc_path" $tmux_path && echo "Symbolic linking successful."
+
+[ -f "$git_path" ] && echo ".git.config file already exists. Aborting." || ln -s "$gitrc_path" $git_path && echo ".git is successfully installed"
 
 grep -q "source ~/.dotfiles/.alias" ~/.bashrc && echo "The file is already linked." || echo "source ~/.dotfiles/.alias" >> ~/.bashrc && echo "Linked successfully."
 
