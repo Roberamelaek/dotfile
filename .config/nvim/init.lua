3eb5fd963f661d3a46f3330ae7d59b2efd74f03a
robera tadesse <robaman2231999@gmail.com>
2024-12-22 21:13:27 -0600

    added all the neccecary plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,162 +1,296 @@
--- General settings
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.autoindent = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.termguicolors = true
-vim.opt.smarttab = true
-vim.opt.softtabstop = 4
-vim.opt.encoding = 'utf-8'
-vim.opt.conceallevel = 2
-vim.o.mouse= ''
---
--- redirct messages to netify
-local notify = require("notify")
-
--- Set nvim-notify as the default notification handler
-vim.notify = notify
-
-require("notify").setup({
-    stages = "fade", -- Animation style
-    timeout = 3000,  -- Time for notification to disappear (in ms)
-    background_colour = "#000000",
-})
-
--- Redirect all Neovim messages to nvim-notify
-local function notify_output()
-    local messages = vim.api.nvim_exec2("messages", { output = true }).output
-    if messages ~= "" then
-        notify(messages, "info", { title = "Neovim Message" })
-    end
+-- Install Lazy.nvim if it's not already installed
+local install_path = vim.fn.stdpath('data')..'/site/pack/lazy/start/lazy.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/folke/lazy.nvim', install_path})
 end
 
--- Hook to display messages after every command
-vim.api.nvim_create_autocmd("CmdlineLeave", {
-    callback = notify_output,
-})
---
--- Enable wildmenu for better command-line autocompletion
-vim.opt.wildmenu = true
-vim.opt.wildmode = 'longest:full,full' -- Completes the longest match first and then shows full matches
 
--- Folding settings for Markdown
-vim.cmd [[
-  autocmd FileType markdown setlocal foldmethod=expr
-  autocmd FileType markdown setlocal foldexpr=MarkdownFoldExpr()
-]]
+-- Configure your plugins using Lazy.nvim
+require("lazy").setup({
 
--- Insert Date command
-vim.api.nvim_create_user_command('InsertDate', function()
-  vim.api.nvim_feedkeys(vim.fn.strftime("%b %d, %y"), 'n', true)
-end, {})
+{
+    "github/copilot.vim",
+    config = function()
+        -- Enable copilot to work with comments and suggestions
+        vim.g.copilot_filetypes = {
+            ["*"] = true,          -- Enable Copilot for all filetypes
+            ["markdown"] = true,   -- Enable Copilot for Markdown
+            ["gitcommit"] = true,  -- Enable Copilot for Git commit messages
+        }
 
--- Airline settings for the status line
-vim.g.airline_powerline_fonts = 1
-vim.g.airline_left_sep = ''
-vim.g.airline_left_alt_sep = ''
-vim.g.airline_right_sep = ''
-vim.g.airline_right_alt_sep = ''
-vim.g.airline_symbols = {
-    branch = '',
-    readonly = '',
-    linenr = ''
-}
+        -- Map `<C-e>` to accept Copilot suggestions
+        vim.api.nvim_set_keymap(
+            "i",
+            "<C-e>",
+            'copilot#Accept("<CR>")',
+            { silent = true, expr = true }
+        )
 
--- Key Mappings
-vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-f>', ':NERDTreeFocus<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-n>', ':NERDTree<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-l>', ':call CocActionAsync("jumpDefinition")<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F8>', ':TagbarToggle<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
+        -- Suggest Copilot inline hints
+        vim.g.copilot_no_tab_map = true -- Use custom keymaps instead of `<Tab>`
+    end,
+},
 
--- Telescope Key Mappings
-local telescope = require('telescope.builtin')
+-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+{
+    'numToStr/Comment.nvim',
+    opts = {
+        padding = true, -- Add a space b/w comment and line
+        -- add any options here
+    }
+},
 
--- Switch between buffers with Telescope
-vim.keymap.set('n', '<leader>bb', telescope.buffers, { desc = 'Switch between buffers' })
+  -- Plugins configuration
+  {
+    "hrsh7th/nvim-cmp", -- Autocompletion plugin
+    dependencies = {
+      "hrsh7th/cmp-buffer", -- Buffer completions
+      "hrsh7th/cmp-path", -- Path completions
+      "hrsh7th/cmp-cmdline", -- Cmdline completions
+      "L3MON4D3/LuaSnip", -- Snippet engine
+      "saadparwaiz1/cmp_luasnip", -- Snippet completions for nvim-cmp
+    },
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
 
--- Navigate between buffers
-vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { desc = 'Next buffer' })
-vim.keymap.set('n', '<leader>bp', ':bprev<CR>', { desc = 'Previous buffer' })
+      -- Enable nvim-cmp completion
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body) -- For luasnip users
+          end,
+        },
+        mapping = {
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-u>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },  -- Snippets from luasnip
+          { name = 'buffer' },  -- Buffer completions
+          { name = 'path' },  -- Path completions
+        }),
+      })
 
--- Close the current buffer
-vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { desc = 'Close current buffer' })
+      -- Set up cmp for command line mode
+      cmp.setup.cmdline(':', {
+        sources = {
+          { name = 'cmdline' },
+        },
+      })
 
--- Confirm selection
-vim.keymap.set('n', '<C-r>', telescope.find_files, { desc = 'Confirm selection' })
-
--- Save all buffers
-vim.keymap.set('n', '<leader>wa', ':wa<CR>', { desc = 'Save all buffers' })
-
--- Enable Markdown-specific fold expression
-function MarkdownFoldExpr()
-  local thisline = vim.fn.getline(vim.fn.line('.'))
-  local nextline = vim.fn.getline(vim.fn.line('.') + 1)
-  if nextline:match('^##') then
-    return '>1'
-  else
-    return '='
-  end
-end
-
--- Calling Lua file to load plugins
-require('plugins')
-
--- Setup nvim-cmp.
-local cmp = require'cmp'
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- Using vsnip or other snippet engines
+      -- Set up cmp for insert mode
+      cmp.setup.cmdline('/', {
+        sources = {
+          { name = 'buffer' },
+        },
+      })
     end,
   },
-  mapping = {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+
+
+
+--------------------------------------
+----------------------------------------
+  {
+    "epwalsh/obsidian.nvim",
+    config = function()
+      require("obsidian").setup({
+        workspaces = {
+          {
+            name = "Self",
+            path = "~/.nb/home",
+          },
+        },
+        completion = {
+          nvim_cmp = true,
+          min_chars = 2,
+        },
+        log_level = vim.log.levels.INFO,
+        wiki_link_func = function(opts)
+          return require("obsidian.util").wiki_link_id_prefix(opts)
+        end,
+        markdown_link_func = function(opts)
+          return require("obsidian.util").markdown_link(opts)
+        end,
+        preferred_link_style = "wiki",  -- or "markdown"
+        disable_frontmatter = false,
+        note_path_func = function(spec)
+          local sanitized_alias = spec.title:gsub(" ", "-"):gsub("[^A-Za-z0-9%-_]", ""):lower()
+          return (spec.dir / sanitized_alias):with_suffix(".md")
+        end,
+        ui = {
+          enable = true,
+          update_debounce = 200,
+          max_file_length = 5000,
+          checkboxes = {
+            [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+            ["x"] = { char = "", hl_group = "ObsidianDone" },
+            [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+            ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+            ["!"] = { char = "", hl_group = "ObsidianImportant" },
+          },
+          bullets = { char = "•", hl_group = "ObsidianBullet" },
+          external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+          reference_text = { hl_group = "ObsidianRefText" },
+          highlight_text = { hl_group = "ObsidianHighlightText" },
+          tags = { hl_group = "ObsidianTag" },
+          block_ids = { hl_group = "ObsidianBlockID" },
+          hl_groups = {
+            ObsidianTodo = { bold = true, fg = "#f78c6c" },
+            ObsidianDone = { bold = true, fg = "#89ddff" },
+            ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+            ObsidianTilde = { bold = true, fg = "#ff5370" },
+            ObsidianImportant = { bold = true, fg = "#d73128" },
+            ObsidianBullet = { bold = true, fg = "#89ddff" },
+            ObsidianRefText = { underline = true, fg = "#c792ea" },
+            ObsidianExtLinkIcon = { fg = "#c792ea" },
+            ObsidianTag = { italic = true, fg = "#89ddff" },
+            ObsidianBlockID = { italic = true, fg = "#89ddff" },
+            ObsidianHighlightText = { bg = "#75662e" },
+          },
+        },
+      })
+    end
   },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'path' },
-    { name = 'buffer' },
-    { name = 'cmdline' },  -- Enable command-line completion
+---------------------------------------
+--------------------------------------
+
+
+
+
+
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      bigfile = { enabled = true },
+      dashboard = {
+        sections = {
+          { section = "terminal", cmd = "chafa ~/.config/wall.png", height = 32, padding = 1 },
+          {
+            pane = 2,
+            { section = "keys", gap = 1, padding = 1 },
+            { section = "startup" },
+          },
+        },
+      },
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true, timeout = 3000 },
+      quickfile = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+      styles = {
+        notification = {}
+      }
+    },
+    keys = {
+      { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+      { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+      { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+      { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+      { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+      { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+      { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+      { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+      { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
+      { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+      { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+      { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
+      { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+      { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+      { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+      { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+      { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+      {
+        "<leader>N",
+        desc = "Neovim News",
+        function()
+          Snacks.win({
+            file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+            width = 0.6,
+            height = 0.6,
+            wo = {
+              spell = false,
+              wrap = false,
+              signcolumn = "yes",
+              statuscolumn = " ",
+              conceallevel = 3,
+            },
+          })
+        end,
+      }
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          -- Setup some globals for debugging (lazy-loaded)
+          _G.dd = function(...)
+            Snacks.debug.inspect(...)
+          end
+          _G.bt = function()
+            Snacks.debug.backtrace()
+          end
+          vim.print = _G.dd -- Override print to use snacks for := command
+
+          -- Create some toggle mappings
+          Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+          Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+          Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+          Snacks.toggle.diagnostics():map("<leader>ud")
+          Snacks.toggle.line_number():map("<leader>ul")
+          Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+          Snacks.toggle.treesitter():map("<leader>uT")
+          Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+          Snacks.toggle.inlay_hints():map("<leader>uh")
+          Snacks.toggle.indent():map("<leader>ug")
+          Snacks.toggle.dim():map("<leader>uD")
+        end,
+      })
+    end,
   },
-}
-
--- Setup cmdline completions
-cmp.setup.cmdline(':', {
-  sources = {
-    { name = 'cmdline' },  -- Command-line completion
-    { name = 'path' },     -- Path completion (for file names, etc.)
-  }
-})
-
--- For other command-line modes like /search or ?search:
-cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' },   -- Search buffer contents
-    { name = 'path' },     -- Path completion
-  }
-})
-
-cmp.setup.cmdline('?', {
-  sources = {
-    { name = 'buffer' },
-    { name = 'path' },
-  }
-})
-
-
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "markdown", "markdown_inline" },
-  highlight = {
-    enable = true,
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { 'junegunn/fzf.vim' },
+  { "nvim-lualine/lualine.nvim" },
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup()
+    end,
+  },
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
 })
+
+-- Optional: You can add your additional Lua configurations here
+require('cmpss')
+require("set")
+
+
+
+
---
+++ .config/nvim/lua/cmpss.lua
@@ -0,0 +1,59 @@
+return {
+  -- Add nvim-cmp and its dependencies
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",  -- LSP completion
+      "hrsh7th/cmp-buffer",    -- Buffer completions
+      "hrsh7th/cmp-path",      -- Path completions
+      "hrsh7th/cmp-cmdline",   -- Command-line completions
+      "L3MON4D3/LuaSnip",      -- Snippet engine
+      "saadparwaiz1/cmp_luasnip", -- Snippet completions
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = {
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+
+      -- Use buffer source for `/` and `?` (search)
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      -- Use cmdline & path source for `:` (command line)
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+    end,
+  },
+}
+
