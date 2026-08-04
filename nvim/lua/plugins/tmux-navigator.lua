a117e8b8ff0cb1687a7eb3a7f5ce8cccf322317e
Robera Tadesse <robaman2231999@gmail.com>
2026-08-04 14:12:00 -0500

    feat(nvim): add vim-tmux-navigator for unified pane movement

---
+++ nvim/lua/plugins/tmux-navigator.lua
@@ -0,0 +1,10 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  lazy = false,
+  keys = {
+    { "<C-h>", "<cmd>NvimTmuxNavigateLeft<CR>" },
+    { "<C-j>", "<cmd>NvimTmuxNavigateDown<CR>" },
+    { "<C-k>", "<cmd>NvimTmuxNavigateUp<CR>" },
+    { "<C-l>", "<cmd>NvimTmuxNavigateRight<CR>" },
+  },
+}
